e6aade9717682a8ab4c1b9eb8a036a4aebf9630a
krtupper <46764559+krtupper@users.noreply.github.com>
2020-03-02 10:15:15 -0700

    add state.java

---
+++ DFA-Final/fa/State.java
@@ -0,0 +1,30 @@
+package fa;
+
+public abstract class State {
+	/**
+	 * The state label.
+	 * It should be a unique name.
+	 * @author elenasherman
+	 */
+	protected String name;
+	
+	/**
+	 * getter for the string label
+	 * @return returns the state label.
+	 */
+	public String getName(){
+		return name;
+	}
+	
+	@Override
+	public String toString(){
+		return name;
+	}
+	
+	public void addState(String state) {
+		
+		name = state;
+	}
+	
+	
+}
